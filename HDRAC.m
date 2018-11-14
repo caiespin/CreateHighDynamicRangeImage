e0d1e532c60bec061439d09f493d59cc2c77e455
Aaron Hunter <aa_hunter@yahoo.com>
2018-11-14 13:06:58 -0800

    made work with power fit to data

--- HDRAC.m
+++ HDRAC.m
@@ -17,17 +17,15 @@ fName = ('Mission Chapel/_DSF1678.JPG');
 im_scale = 255/pixel_fit(255,1,r_inv,b_inv,b_inv,scale);
 %fName = ('Bike/_DSF1704.JPG')
 Pic1 = imread(fName);
-x_max = size(Pic1, 1);
-y_max = size(Pic1, 2);
-color_max = 3;
-scale = 255.0;
-Pic1_norm = double(Pic1);
-Pic1_lin = PolyLin(Pic1_norm,r_inv,b_inv,b_inv,scale);
-Pic1_lin = Pic1_lin*im_scale;
+[Pic1_lin(:,:,1), gR, sR] = Linearization(Pic1(:,:,1), mean_RF, T);
+[Pic1_lin(:,:,2), gG, sG] = Linearization(Pic1(:,:,2), mean_GF, T);
+[Pic1_lin(:,:,3), gB, sB] = Linearization(Pic1(:,:,3), mean_BF, T);
+GScale = max([sR sG sB]);
+%Pic1_lin = Pic1_lin*im_scale;
 
 figure()
 subplot(3,2,[1 3 5])
-imshow(uint8(Pic1_lin));  
+imshow(uint8(Pic1_lin/GScale));  
 subplot(3,2,2)
 histogram(Pic1_lin(:,:,1))
 subplot(3,2,4)
@@ -39,17 +37,12 @@ histogram(Pic1_lin(:,:,3))
 fName = ('Mission Chapel/_DSF1674.JPG');
 %fName = ('Bike/_DSF1702.JPG')
 Pic2 = imread(fName);
-x_max = size(Pic2, 1);
-y_max = size(Pic2, 2);
-color_max = 3;
-Pic2_norm = double(Pic2);
-Pic2_lin = PolyLin(Pic2_norm,r_inv,b_inv,b_inv,scale);
-Pic2_lin = Pic2_lin*im_scale;
-Pic2_lin = threshold(Pic2_lin);
-
+[Pic2_lin(:,:,1), gR, sR] = Linearization(Pic2(:,:,1), mean_RF, T);
+[Pic2_lin(:,:,2), gG, sG] = Linearization(Pic2(:,:,2), mean_GF, T);
+[Pic2_lin(:,:,3), gB, sB] = Linearization(Pic2(:,:,3), mean_BF, T);
 figure()
 subplot(3,2,[1 3 5])
-imshow(uint8(Pic2_lin));  
+imshow(uint8(Pic2_lin/GScale));  
 subplot(3,2,2)
 histogram(Pic2_lin(:,:,1))
 subplot(3,2,4)
@@ -60,17 +53,12 @@ histogram(Pic2_lin(:,:,3))
 fName = ('Mission Chapel/_DSF1676.JPG')
 %fName = ('Bike/_DSF1701.JPG')
 Pic3 = imread(fName);
-x_max = size(Pic3, 1);
-y_max = size(Pic3, 2);
-color_max = 3;
-Pic3_norm = double(Pic3);
-Pic3_lin = PolyLin(Pic3_norm,r_inv,b_inv,b_inv,scale);
-Pic3_lin = Pic3_lin*im_scale;
-Pic3_lin = threshold(Pic3_lin);
-
+[Pic3_lin(:,:,1), gR, sR] = Linearization(Pic3(:,:,1), mean_RF, T);
+[Pic3_lin(:,:,2), gG, sG] = Linearization(Pic3(:,:,2), mean_GF, T);
+[Pic3_lin(:,:,3), gB, sB] = Linearization(Pic3(:,:,3), mean_BF, T);
 figure()
 subplot(3,2,[1 3 5])
-imshow(uint8(Pic3_lin));  
+imshow(uint8(Pic3_lin/GScale));  
 subplot(3,2,2)
 histogram(Pic3_lin(:,:,1))
 subplot(3,2,4)
@@ -86,7 +74,7 @@ a(1) = 1;
 a(2) = t(2)/t(1); %scale factor between image two and one
 a(3) = t(3)/t(1); %scale factor betweem image three and image one
 %composition 1
-comp1HDR = pic_combine(Pic1_lin,Pic2_lin,Pic3_lin,a);
+comp1HDR = ImMerge1(Pic1_lin,Pic2_lin,Pic3_lin,a);
 comp1 = tonemap(comp1HDR,'AdjustSaturation',2.5, 'AdjustLightness', [0.15 1]);
 figure()
 comp1_rsize = imresize(comp1, 0.35);
--- ImMerge1.m
+++ ImMerge1.m
@@ -9,6 +9,7 @@ mask(:,:,3) = double(maskAll);
 %mask for the first image where it has the highest exposure pixels 
 mask1 = mask;
 
+%mask the lowest exposure pixels
 mask = im1 < E_max/a(3);
 maskAll = mask(:,:,1).*mask(:,:,2).*mask(:,:,3);
 mask(:,:,1) = double(maskAll);
@@ -16,11 +17,12 @@ mask(:,:,2) = double(maskAll);
 mask(:,:,3) = double(maskAll);
 mask3 = mask;
 
+%mask two is the inverse of mask1 and mask3
 mask2 = not(mask1) .* not(mask3);
 
 Image1 = (im1.*mask1);
-Image2 = (im1.*mask2 + im2.*mask2/a(2))/2;
-Image3 = (im1.*mask3 + (im2.*mask3)/a(2) +(im3.*mask3)/a(3))/3;
+Image2 = (im2.*mask2/a(2));
+Image3 = ((im3.*mask3)/a(3));
 
 subplot(1,4,1)
 imshow(uint8(mask1*255));
